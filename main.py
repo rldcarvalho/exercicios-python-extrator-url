3e08e66d70f82e111aca64b0c546f5910f9ce390
Rafael Carvalho <rafa_rafael_lucas@hotmail.com>
2022-05-31 21:40:17 -0300

    adição de funcionalidades main.py
    Sanitização da URL;
    Validação da URL;
    Separação da URL em base e parâmetros;
    Método para busca de um valor de um parâmetro.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
+url = "bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 print(url)
 
-url_base = url[0:19]
-print(url_base)
+#Sanitização da URL
+url = url.strip
 
-url_parametros = url[20:36]
+#Validação da URL
+if url == ""
+    raise ValueError("A URL está vazia")
+
+#Separa base e os parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+print(url_base)
+url_parametros = url[indice_interrogacao + 1:]
 print(url_parametros)
+
+#Busca o valor de um parâmetro
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+valor = url_parametros[indice_valor:] if indice_e_comercial == -1 else url_parametros[indice_valor:indice_e_comercial]
+print(valor)
